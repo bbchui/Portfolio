f7faeeff8f1522e59ffbb1d583c18d50cafa3a14
Brandon Chui <bbchui325@gmail.com>
2017-08-09 03:55:44 -0700

    icons now open new tab instead of redirect

--- src/footer.jsx
+++ src/footer.jsx
@@ -12,11 +12,12 @@ class Footer extends React.Component {
     return(
       <div className="footer">
         <ul className="social-icons">
-          <li><a href="https://www.facebook.com/bbchui"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-          <li><a href="https://angel.co/brandon-chui"><i className="fa fa-angellist" aria-hidden="true"></i></a></li>
+          <li><a href="https://www.github.com/bbchui" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+
+          <li><a href="https://www.linkedin.com/in/brandon-chui/" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
           <li><a href="mailto:bbchui325@gmail.com?subject=Let's Chat!"><i className="fa fa-envelope-o" aria-hidden="true"></i></a></li>
-          <li><a href="https://www.linkedin.com/in/brandon-chui/"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-          <li><a href="https://www.github.com/bbchui"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+          <li><a href="https://angel.co/brandon-chui" target="_blank"><i className="fa fa-angellist" aria-hidden="true"></i></a></li>
+          <li><a href="https://www.facebook.com/bbchui" target="_blank"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
         </ul>
       </div>
     )
