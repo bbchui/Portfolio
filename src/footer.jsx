c79586d83f3f67ef33518b3ab0916e8388f56acf
Brandon Chui <bbchui325@gmail.com>
2017-08-09 03:42:25 -0700

    setup social icons

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
 
 class Footer extends React.Component {
   constructor(props) {
@@ -9,13 +11,12 @@ class Footer extends React.Component {
   render() {
     return(
       <div className="footer">
-        <ul class="list-inline nipp-social-icon">
-          <li><a href="https://www.facebook.com/achen1231"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-          <li><a href="https://www.instagram.com/"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
-
-          <li><a href="mailto:alchen1231@gmail.com?subject=Let's Chat!"><i className="fa fa-envelope-o" aria-hidden="true"></i></a></li>
-          <li><a href="https://www.linkedin.com/in/alchen1231/"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-          <li><a href="https://www.github.com/alnchen"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+        <ul className="social-icons">
+          <li><a href="https://www.facebook.com/bbchui"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+          <li><a href="https://angel.co/brandon-chui"><i className="fa fa-angellist" aria-hidden="true"></i></a></li>
+          <li><a href="mailto:bbchui325@gmail.com?subject=Let's Chat!"><i className="fa fa-envelope-o" aria-hidden="true"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/brandon-chui/"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+          <li><a href="https://www.github.com/bbchui"><i className="fa fa-github" aria-hidden="true"></i></a></li>
         </ul>
       </div>
     )
