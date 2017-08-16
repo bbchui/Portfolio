5bb6170bdeef01ff1532428fb6f3895c79dbfda3
Brandon Chui <bbchui325@gmail.com>
2017-08-16 15:09:18 -0700

    email validation

--- src/contact.jsx
+++ src/contact.jsx
@@ -5,16 +5,19 @@ import axios from 'axios';
 class Contact extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {name: "", email: "", message: "", error: false}
+    this.state = {name: "", email: "", message: "", error: 0}
     this.update = this.update.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleError = this.handleError.bind(this);
+    this.invalidEmail = this.invalidEmail.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
     if (!this.state.name || !this.state.email || !this.state.message) {
-      this.setState({error: true})
+      this.setState({error: 1})
+    } else if (this.invalidEmail(this.state.email)) {
+      this.setState({error: 2})
     } else {
       this.setState({error: false})
       axios({
@@ -32,11 +35,30 @@ class Contact extends React.Component {
     this.setState({name: "", email: "", message: ""})
   }
 
+  invalidEmail(email) {
+    let arr;
+    if (email.indexOf("@") === -1) {
+      return true;
+    } else {
+      arr = email.split("@")
+    }
+    if (arr.length != 2) {
+      return true;
+    } else if (arr[1].split(".") != 2) {
+      return true;
+    }
+    return false;
+  }
+
   handleError() {
-    if (this.state.error) {
+    if (this.state.error === 1) {
       return(
         <div>Please complete the form!</div>
       );
+    } else if (this.state.error === 2) {
+      return(
+        <div>Please enter a valid e-mail</div>
+      )
     }
   }
 
